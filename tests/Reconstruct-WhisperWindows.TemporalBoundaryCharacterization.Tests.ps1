2f3af3fc9d4421ee0262ece3f3dce08d5ae4d5d0
Drworm82 <worm_jim@hotmail.com>
2026-09-10 23:18:42 -0600

    Correct reconstruction boundary characterization

--- tests/Reconstruct-WhisperWindows.TemporalBoundaryCharacterization.Tests.ps1
+++ tests/Reconstruct-WhisperWindows.TemporalBoundaryCharacterization.Tests.ps1
@@ -3,22 +3,16 @@ Describe "Reconstruct-WhisperWindows MATCH placement boundary" {
         $prefix = @(
             [PSCustomObject]@{ Key='and'; Text='And'; From=2.84; To=4.00; Id=100 }
         )
-
         $currentMatch = @(
             [PSCustomObject]@{ Key='i'; Text='I'; From=4.00; To=4.11; Id=201 }
             [PSCustomObject]@{ Key='would'; Text='would'; From=4.11; To=4.66; Id=202 }
             [PSCustomObject]@{ Key='like'; Text='like'; From=4.66; To=5.07; Id=203 }
         )
-
         $newFinal = @($prefix) + @($currentMatch)
         $orderViolations = 0
-
         for ($i = 1; $i -lt $newFinal.Count; $i++) {
-            if ($newFinal[$i].From -lt $newFinal[$i - 1].To) {
-                $orderViolations++
-            }
+            if ($newFinal[$i].From -lt $newFinal[$i - 1].To) { $orderViolations++ }
         }
-
         $newFinal.Count | Should Be 4
         $newFinal[0].Text | Should Be 'And'
         $newFinal[1].Text | Should Be 'I'
@@ -30,21 +24,15 @@ Describe "Reconstruct-WhisperWindows MATCH placement boundary" {
         $prefix = @(
             [PSCustomObject]@{ Key='participate'; Text='participate'; From=3.80; To=4.13; Id=300 }
         )
-
         $currentMatch = @(
             [PSCustomObject]@{ Key='in'; Text='in'; From=4.00; To=4.22; Id=401 }
             [PSCustomObject]@{ Key='this'; Text='this'; From=4.22; To=4.66; Id=402 }
         )
-
         $newFinal = @($prefix) + @($currentMatch)
         $orderViolations = 0
-
         for ($i = 1; $i -lt $newFinal.Count; $i++) {
-            if ($newFinal[$i].From -lt $newFinal[$i - 1].To) {
-                $orderViolations++
-            }
+            if ($newFinal[$i].From -lt $newFinal[$i - 1].To) { $orderViolations++ }
         }
-
         $newFinal[0].Text | Should Be 'participate'
         $newFinal[1].Text | Should Be 'in'
         $newFinal[1].From | Should Be 4.00
